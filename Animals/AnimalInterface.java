026a131bde4bc8057cae5a2642de608606b24c73
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-10-03 21:24:21 +0300

    Create AnimalInterface.java

---
+++ Animals/AnimalInterface.java
@@ -0,0 +1,83 @@
+import java.util.List;
+
+public class AnimalInterface {
+
+    /**
+     * Получить имя животного
+     *
+     * @return
+     */
+    String getName() {
+        return null;
+    }
+
+
+    /**
+     * Установить дату рождения
+     *
+     * @param date
+     */
+    void setDateBirth(String date) {
+
+    }
+
+
+    /**
+     * Получить дату рождения
+     *
+     * @return
+     */
+    String getDateBirth() {
+        return null;
+    }
+
+
+    /**
+     * Установить имя животного
+     *
+     * @param name
+     */
+    void setName(String name) {
+
+    }
+
+
+    /**
+     * Добавить команду
+     *
+     * @param newCommand
+     */
+    void addCommand(String newCommand) {
+
+    }
+
+
+    /**
+     * Удаление команды
+     *
+     * @param command
+     */
+    void removeCommand(String command) {
+
+    }
+
+
+    /**
+     * Список команд
+     *
+     * @return
+     */
+    List<String> getCommandList() {
+        return null;
+    }
+
+
+    /**
+     * Количество команд животного
+     *
+     * @return
+     */
+    int getCommandCount() {
+        return 0;
+    }
+}
